265dddafcb5550c4a04e1464892abde2c9862685
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-20 12:42:59 -0500

    add task partial, index view, task helper functions for styling, add style to application layout

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,19 @@
 module TasksHelper
+  def category_color(category)
+    colors = { 
+      "work": "bg-emerald-400 text-white", 
+      "personal": "bg-fuchsia-400 text-white", 
+      "school": "bg-cyan-400 text-white" 
+    }
+    colors[category.to_sym] unless category.nil?
+  end
+
+  def priority_color(priority)
+    colors = { 
+      "low": "bg-green-200", 
+      "medium": "bg-yellow-200", 
+      "high": "bg-red-200" 
+    }
+    colors[priority.to_sym] unless priority.nil?
+  end
 end
