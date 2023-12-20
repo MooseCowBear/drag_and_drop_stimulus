9bce73d2397c4931d3208166ca9380e68dea8f50
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-20 17:08:17 -0500

    add tasks controller, remove field with errors div, add task views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,46 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = Task.all
   end
 
   def show
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
   end
 
   def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
   end
 
   def destroy
+    @task.destroy
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :category, :priority)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Task.create!(title: "Group project guidelines", description: "Need to think of some guidelines for productive group work", category: 2)
+Task.create!(title: "Presents?", description: "all the nieces and nephews will need gifts..", category: 1, priority: 1)
+Task.create!(title: "Brainstorming session", description: "need new ideas for product launch", category: 0, priority: 2)
+Task.create!(title: "Call Vet for Emma's physical", priority: 2)
