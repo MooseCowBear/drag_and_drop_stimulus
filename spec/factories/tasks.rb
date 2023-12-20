d0fcb17306368a1887c8931e8043e2793d71b6ee
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-20 10:34:53 -0500

    add validation tests for task model

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
-    title { "MyString" }
-    description { "MyString" }
+    title { "task" }
+    description { "task description" }
     category { 1 }
     priority { 1 }
   end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid when it has a title" do
+    task = create(:task)
+    expect(task).to be_valid
+  end
+
+  it "is not valid when title is missing" do
+    task = build(:task, title: "")
+    expect(task).not_to be_valid
+  end
 end
