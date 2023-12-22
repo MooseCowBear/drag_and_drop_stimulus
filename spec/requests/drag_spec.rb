4ceed2616bed115df5d8d91b9e9420041a71482b
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-22 12:06:41 -0500

    add drag request tests

--- spec/requests/drag_spec.rb
+++ spec/requests/drag_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Drags", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /drag" do
+    before(:each) do
+      @task0 = create(:task)
+      @task1 = create(:task, position: 1)
+      @task2 = create(:task, position: 2)
+    end
+
+    it "updates the positions of tasks whose ids are included in params" do
+      post drag_url, params: {drag: { category: "tasks", ids: [@task0.id, @task1.id], positions: [1, 0] } }
+      @task0.reload
+      @task1.reload
+      expect(@task0.position).to eq(1)
+      expect(@task1.position).to eq(0)
+    end
+
+    it "does not update tasks whose ids are not included in params" do
+      post drag_url, params: {drag: { category: "tasks", ids: [@task0.id, @task1.id], positions: [1, 0] } }
+      @task2.reload
+      expect(@task2.position).to eq(2)
+    end
   end
 end
